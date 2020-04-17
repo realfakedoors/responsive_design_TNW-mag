bfd8a28b995789ef0bdf7c32ba67caebae2c54fa
Andy Holt <andyholt85@gmail.com>
2020-04-17 03:24:58 -0600

    built basic layout

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+  // When the user scrolls the page, execute scrollNav
+  window.onscroll = function() {scrollNav()};
+
+  // Get the navbar
+  var navbar  = document.getElementById("fixed-navbar");
+  
+  // Get the department navbar's logo
+  var navlogo = document.getElementById("nav-logo");
+
+  // Get the offset position of the navbar
+  var sticky = navbar.offsetTop;
+
+  // Add the sticky class to the navbar and show the navlogo
+  // when a user reaches its scroll position.
+  
+  // Remove "sticky" and hide the navlogo when a user scrolls above the navbar.
+  function scrollNav() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+      navlogo.style.display = "inline-block";
+    } else {
+      navbar.classList.remove("sticky");
+      navlogo.style.display = "none";
+    }
+  }
+  
+})
